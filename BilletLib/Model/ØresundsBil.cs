52cfda141a3562e04b3b5f1c001f2ac92db5ec99
Kasper Jensen <rtskasper@gmail.com>
2017-09-02 19:46:34 +0200

    Opgave 8 uden tests er oprettet

---
+++ BilletLib/Model/ØresundsBil.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilletLib.Model
+{
+    public class ØresundsBil : IKøretøj
+    {
+        public string Nummerplade { get; set; }
+        public DateTime Dato { get; set; }
+        public bool BroBizz { get; set; }
+        public int FastPris = 410;
+        public int Pris()
+        {
+            
+            if (BroBizz == true)
+            {
+                FastPris = 161;
+            }
+            return FastPris;
+        }
+
+        public string Køretøj()
+        {
+           return "Øresund Bil";
+        }
+    }
+}
---
+++ BilletLib/Model/ØresundsMC.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilletLib.Model
+{
+    public class ØresundsMC : IKøretøj
+    {
+        public string Nummerplade { get; set; }
+        public DateTime Dato { get; set; }
+        public bool BroBizz { get; set; }
+        public int FastPris = 210;
+        public int Pris()
+        {
+            if (BroBizz == true)
+            {
+                FastPris = 73;
+            }
+            return FastPris;
+        }
+
+        public string Køretøj()
+        {
+            return "Øresund MC";
+        }
+    }
+}
